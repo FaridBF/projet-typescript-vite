40d82da16aafa3ac4e14b85684ffac0418169260
FaridBF <bouras.farid@outlook.fr>
2024-01-11 10:06:59 +0100

    feat: add user.ts

--- src/services/user.ts
+++ src/services/user.ts
@@ -4,3 +4,13 @@ import user from '../mocks/user.json';
 export function getUser(): Promise<UserInterface> {
   return new Promise((resolve) => resolve(user));
 }
+
+type upadatableUserProps = Partial<Omit<UserInterface, 'id'>>;
+
+export function updateUser(updatedUser: upadatableUserProps) {
+  return new Promise<UserInterface>((resolve) =>
+    resolve({ ...user, ...updateUser })
+  );
+}
+
+updateUser({ username: '', id: 3 });
